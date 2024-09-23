e241a1f272c98bc802be219cd816f3a5e663f6fb
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-09-23 14:38:43 -0700

    PASS: batch processing on genomes, genomes file

--- hmmer.py
+++ hmmer.py
@@ -2,20 +2,29 @@
 import os
 import re
 import sys
+import numpy as np
 from pathlib import Path
 from typing import Iterable, Union
 from functools import partial
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 
 from pyhmmer import hmmsearch
 from pyhmmer.easel import SequenceFile
 from pyhmmer.plan7 import HMM, HMMFile
 
-QUERIES_DIR = Path(sys.argv[1])
-OUT_FILE = Path(sys.argv[2])
-WORKERS = int(sys.argv[3])
-TOP_HITS = sys.argv[4] == "True"  # Include Top Hits Object
-GENOMES = sys.argv[5:]
+# QUERIES_DIR = Path(sys.argv[1])
+# OUT_FILE = Path(sys.argv[2])
+# BATCH = int(Path(sys.argv[3]))
+# WORKERS = int(float(sys.argv[4]) * cpu_count())
+# TOP_HITS = sys.argv[5] == "True"  # Include Top Hits Object
+# PATHS_FILE = sys.argv[6]
+
+QUERIES_DIR = Path("queries")
+OUT_FILE = Path("test0.tsv")
+BATCH_SIZE = 512
+WORKERS = int(2 * cpu_count())
+TOP_HITS = False  # Include Top Hits Object
+GENOMES_FILE = Path("genomes.txt")
 
 GENOME_REGEX = re.compile(r"(GCF_\d+\.\d)\.faa$")
 
@@ -59,41 +68,54 @@ def parse_hit(hit):
     return "\t".join(out) + "\n"
 
 
-def run_genome(genome_path, hmms_files):
+def run_genomes(genome_paths, hmms_files):
 
-    genome_id = parse_genome(genome_path)
+    def run_genome(genome_path):
+        genome_id = parse_genome(genome_path)
 
-    out = {}
-    out[genome_id] = {"tsv": None, "top_hits": None}
+        out = {}
+        out[genome_id] = {"tsv": None, "top_hits": None}
 
-    with SequenceFile(genome_path, digital=True) as genome_file:
-        genome = genome_file.read_block()
-        results = hmmsearch(hmms_files, genome)
+        with SequenceFile(genome_path, digital=True) as genome_file:
+            genome = genome_file.read_block()
+            results = hmmsearch(hmms_files, genome)
 
-    tsv = list()
-    for top_hits in results:
-        for hit in top_hits:
-            parsed = parse_hit(hit)
-            hit_tsv = f"{genome_id}\t{parsed}"
-            tsv.append(hit_tsv)
+        tsv = []
+        for top_hits in results:
+            for hit in top_hits:
+                parsed = parse_hit(hit)
+                hit_tsv = f"{genome_id}\t{parsed}"
+                tsv.append(hit_tsv)
 
-    if TOP_HITS:
-        out[genome_id]["top_hits"] = results
-    else:
-        del results
+        if TOP_HITS:
+            out[genome_id]["top_hits"] = results
+        else:
+            del results
 
-    out[genome_id]["tsv"] = tsv
+        out[genome_id]["tsv"] = tsv
 
-    return out
+        return out
+
+    merged = {}
+    for resultD in map(run_genome, genome_paths):
+        merged |= resultD
+
+    return merged
 
 
 if __name__ == "__main__":
 
+    with open(GENOMES_FILE, "r") as genomes_file:
+        genomes_paths = [Path(line.rstrip()) for line in genomes_file]
+        batches = np.array_split(np.array(genomes_paths), BATCH_SIZE)
+
     hmms_files = get_hmms(QUERIES_DIR)
-    worker = partial(run_genome, hmms_files=hmms_files)
+    worker = partial(run_genomes, hmms_files=hmms_files)
 
     with Pool(WORKERS) as pool:
-        results = pool.map(worker, GENOMES)
+        results = pool.imap_unordered(worker, batches)
+        pool.close()
+        pool.join()
 
     # merge dictionaries
     merged = {}
